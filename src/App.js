395e23852b21c90e0f81d122a8a7f2f1a721550f
Rishabh Varshney <rishabhvarshney14@gmail.com>
2021-08-16 17:26:21 +0530

    Added About Component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 // Custom Components
 import Header from "./components/header/Header";
 import Projects from "./components/projects/Projects";
+import About from "./components/about/About";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Projects />
+      <About />
     </div>
   );
 }
---
+++ src/components/about/About.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+// Data
+import ProfileImage from "../../data/images/profile.png";
+
+// Styles
+import styles from "./styles/about.module.css";
+
+const About = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <img className={styles.profileImg} src={ProfileImage} alt="profile" />
+      </div>
+      <div className={styles.textField}>
+        <h1 className={styles.heading}>About Me</h1>
+        <p className={styles.textLine}>
+          I am a passionate, curious programmer who never wants to stop
+          learning.
+        </p>
+        <p className={styles.textLine}>
+          I am currently an undergrad student majoring in Computer Science and
+          Engineering.
+        </p>
+        <p className={styles.textLine}>
+          I consider myself a curious and inquisitive person, so in my spare
+          time, I like to work on side projects and try to constantly learn
+          something new to improve my skillset.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
